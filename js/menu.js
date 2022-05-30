5959b61e8c9743bddc19005f56708670e903851a
Vetali1985 <geelycross1985@gmail.com>
2022-05-30 19:42:00 +0300

    30.05

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,30 @@
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector(".btn-menu-mob"),
+//     closeMenuBtn: document.querySelector(".btn-menu-close"),
+//     menu: document.querySelector(".mobile-menu"),
+//     body: document.querySelector("body"),
+//   };
+
+//   refs.openMenuBtn.addEventListener("click", toggleMenu);
+//   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle("is-hidden");
+//     refs.body.classList.toggle("no-scroll");
+//   }
+// })();
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector(".btn-menu-mob"),
-    closeMenuBtn: document.querySelector(".btn-menu-close"),
-    menu: document.querySelector(".mobile-menu"),
-    body: document.querySelector("body"),
-  };
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll");
-  }
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
 })();
